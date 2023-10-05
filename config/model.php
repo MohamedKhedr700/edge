e9990329b89e0b7b62bbb57b86e0f1680ee7ac15
mohamedkhedr700 <M.khedr@rowaad.net>
2023-10-06 01:57:36 +0300

    first commit

---
+++ config/model.php
@@ -0,0 +1,26 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Model Manager
+    |--------------------------------------------------------------------------
+    | Here you may specify the default model manager.
+    |
+    |
+    */
+
+    'model_manager' => \Raid\Core\Model\Models\Model::class,
+
+    /*
+    |--------------------------------------------------------------------------
+    | Base Model
+    |--------------------------------------------------------------------------
+    |
+    | This option allows you to specify the base model for model_manager.
+    |
+    */
+
+    'base_model' => Jenssegers\Mongodb\Eloquent\Model::class,
+];
